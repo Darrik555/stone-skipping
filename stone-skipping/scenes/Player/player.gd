eaf6f08628fc75d28cae54057e5054cf195010e7
Darrik555 <you@example.com>
2025-11-08 20:57:28 +0100

    move arm into player scene

--- stone-skipping/scenes/Player/player.gd
+++ stone-skipping/scenes/Player/player.gd
@@ -1,23 +1,22 @@
-extends Node3D
+class_name Player
+extends CharacterBody3D
 
-@onready var animation_player : AnimationPlayer = $"../AnimationPlayer"
-@onready var progress_bar : ProgressBar = $"../CanvasLayer/ProgressBar"
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@export var progress_bar: ProgressBar
 
-@onready var stone = $"../Stone"
-@onready var camera_3d = $"../Camera3D"
-
+#@onready var stone = $"../Stone"
+@export var camera_3d: Camera3D
 
 var throw_power : float = 0.0
 var throw_direction : Vector3 = Vector3(1,0.1,1)
 var charging : bool = true
 
+var inventory: Inventory = Inventory.new()
 
+func on_throwable_item_bought(throwable: Throwable):
+	inventory.add_throwable(throwable)
+	
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_pressed("leftclick"):
 		#start_throw()
@@ -42,13 +41,15 @@ func _process(_delta):
 
 func throw():
 	
+	var stone = $"../Stone"
+	
 	throw_direction = camera_3d.global_position.direction_to(stone.global_position)
-	throw_direction.y = -throw_direction.y *0.2
+	throw_direction.y = -throw_direction.y * 0.2
 	
 	stone.global_position = global_position
 	
 	stone.velocity = throw_power * throw_direction
-	stone.bounces = stone.max_bounces
+	stone.stats.bounces = stone.stats.max_bounces
 	stone.get_node("CollisionShape3D").disabled = false
 	stone.get_node("State Machine").reset()
 	
