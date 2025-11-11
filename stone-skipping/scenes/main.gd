4dc1274e6ba4eb81004398d160259a25b961c320
Finn <118118221+KarsaO@users.noreply.github.com>
2025-11-11 22:12:32 +0100

    player movement, rotate throw not work

--- stone-skipping/scenes/main.gd
+++ stone-skipping/scenes/main.gd
@@ -3,13 +3,19 @@ extends Node3D
 @export var throwable: Item
 
 @onready var camera_3d: Camera3D = $Camera3D
+@onready var player = %Player
 
 var current_throwable: CharacterBody3D = null
 
 func _ready():
 	pass
- 
-func start_new_throw(power: float, direction: Vector3, player: Player):
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept"):
+		reset_camera()
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func start_new_throw(power: float, direction: Vector3):
 	if not player.consume_throwable():
 		#print("inv empty")
 		return
@@ -24,5 +30,10 @@ func start_new_throw(power: float, direction: Vector3, player: Player):
 	if camera_3d:
 		if camera_3d.has_method("set_target"):
 			camera_3d.set_target(current_throwable)
+			camera_3d.current = true
 	
 	current_throwable.velocity = power * direction
+
+func reset_camera():
+	#camera_3d.set_target(player)
+	player.camera_3d_player.current = true
